bced02ccf1ea3b1836ccbd4ce1341c980b8f2890
Quentin Monmert <qmonmert@qmonmert.home>
2018-12-23 17:23:04 +0100

    Import lib activities in apps strava and garmin

--- apps/garmin/src/app/app.module.ts
+++ apps/garmin/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
+import { ActivitiesModule } from '@sport/activities';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    ActivitiesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
